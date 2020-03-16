58c710a2d26a8eb6f6c6b5851f52fa22bfae15a3
AlinaMikomissa <58024079+AlinaMikomissa@users.noreply.github.com>
2020-03-16 20:10:27 +0200

    Додано функції та тести

--- laba4Java/src/test/java/Cage_mammalsTest.java
+++ laba4Java/src/test/java/Cage_mammalsTest.java
@@ -8,7 +8,47 @@ import org.junit.rules.ExpectedException;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class Cage_mammalsTest {
-   
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+    @Test(expected = CustomException.class)
+    public void ShouldThrowExceptionWhenItIsNoPlace_PutAnimalInCage(){
+        System.out.println("--------------------");
+        Cage_mammals cageM = new Cage_mammals(1);
+        Zebra zb1 = new Zebra("Deli", 4);
+        Zebra zb2 = new Zebra("Vinki", 5);
+        cageM.PutInCage(zb1);
+        cageM.PutInCage(zb2);
+
+    }
+    @Test
+    public void AllShouldBeOK_PutAnimalInCage() {
+        System.out.println("--------------------");
+        Cage_mammals cageM = new Cage_mammals(2);
+        Zebra zb1 = new Zebra("Deli", 4);
+        Zebra zb2 = new Zebra("Vinki", 5);
+        cageM.PutInCage(zb1);
+        cageM.PutInCage(zb2);
+
+    }
+    @Test
+    public void AllShouldBeOK_GetAnimalFromCage(){
+        System.out.println("--------------------");
+        Cage_mammals cageM = new Cage_mammals(2);
+        Zebra zb1 = new Zebra("Deli", 4);
+        cageM.PutInCage(zb1);
+        cageM.GetFromCage(zb1);
+
+
+    }
+    @Test(expected = CustomException.class)
+    public void ShouldThrowExceptionWhenThereIsNotThatAnimalInCage_GetAnimalFromCage(){
+        System.out.println("--------------------");
+        Cage_mammals cageM = new Cage_mammals(2);
+        Zebra zb1 = new Zebra("Deli", 4);
+        cageM.PutInCage(zb1); cageM.GetFromCage(zb1);
+        cageM.GetFromCage(zb1);
+
+    }
 //Errors/////Errors/////////ERRORS////////////////////////////////////////
     /*@Test
     void putInCage_Zebra_and_Bird() {
